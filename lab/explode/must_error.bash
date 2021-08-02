4fa610fb45ebcc694ef8b307773413676cb1174d
chenshun00 <chenshun00@gmail.com>
2021-08-02 20:50:37 +0800

    fix output is error

--- lab/explode/must_error.bash
+++ lab/explode/must_error.bash
@@ -8,6 +8,6 @@ cd target || exit
 unzip demo-0.0.1-SNAPSHOT.jar -d ff > /dev/null 2>&1
 cd ff || exit
 cp ../../lib/no-such-method-error-1.0-SNAPSHOT.jar BOOT-INF/lib
-ls -al no-such*
+ls -al BOOT-INF/lib/no-such-method-error*
 echo '开始启动'
 java org.springframework.boot.loader.PropertiesLauncher
--- lab/explode/normal.bash
+++ lab/explode/normal.bash
@@ -7,6 +7,6 @@ mvn clean compile package
 cd target || exit
 unzip demo-0.0.1-SNAPSHOT.jar -d ff > /dev/null 2>&1
 cd ff || exit
-ls -al no-such*
+ls -al BOOT-INF/lib/no-such-method-error*
 echo '开始启动'
 java org.springframework.boot.loader.PropertiesLauncher
